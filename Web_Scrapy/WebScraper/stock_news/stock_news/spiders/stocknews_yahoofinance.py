9be14515f6f37d7a39d94a71deec994465a04cfd
Thi Van Anh Duong <anht@cbh.com.au>
2024-07-31 21:59:26 +0800

    Scrape News Successfully but facing limit request error !

--- Web_Scrapy/WebScraper/stock_news/stock_news/spiders/stocknews_yahoofinance.py
+++ Web_Scrapy/WebScraper/stock_news/stock_news/spiders/stocknews_yahoofinance.py
@@ -7,16 +7,17 @@ class StockSpider(scrapy.Spider):
     start_urls = ['https://stockanalysis.com/list/australian-securities-exchange/']
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+        'DOWNLOAD_DELAY': 2,
     }
 
     def parse(self, response):
         # Extract links to individual stock pages using XPath
         stock_links = response.xpath('//*[@id="main-table"]/tbody/tr/td[2]/a/@href').getall()
-        i = 0
+        # i = 0
         for link in stock_links:
-            i += 1
-            if i < 30 or i > 60:
-                continue
+            # i += 1
+            # if i < 30 or i > 60:
+            #     continue
 
             stock_url = response.urljoin(link + 'financials/')
             # yield scrapy.Request(stock_url, callback=self.parse_stock)
@@ -34,11 +35,11 @@ class StockSpider(scrapy.Spider):
     def parse_news_content(self, response):
         title = response.xpath('//h1[@id="caas-lead-header-undefined" and @data-test-locator="headline"]/text()').get()
         paragraphs = response.xpath('/html/body/div[2]/div/div/div/main/div/div[1]/div/div/div/div/div/article/div/div/div/div/div/div/div[2]/div[2]/p/text()').extract()
-        if not os.path.exists('raw_news'):
-            os.makedirs('raw_news')
+        if not os.path.exists('news'):
+            os.makedirs('news')
         content = title + '\n\n' + '\n'.join(paragraphs)
         company_name = response.meta['company_name']
-        file_path = f'raw_news/{company_name}.txt'
+        file_path = f'news/{company_name}.txt'
         with open(file_path, 'a', encoding='utf-8') as f:
             f.write(content)
             f.write('\n' + '='*80 + '\n\n')
