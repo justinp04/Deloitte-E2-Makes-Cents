a1b2e7f5b31a38267d989789565cfd3a6fd58594
Thi Van Anh Duong <anht@cbh.com.au>
2024-08-10 22:33:03 +0800

    Timer for Company profile

--- CloudScrapy/scraped_stock.py
+++ CloudScrapy/scraped_stock.py
@@ -11,8 +11,21 @@ scraped_stock = func.Blueprint()
 def ScrapedStockData(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    logging.info('Python HTTP trigger function processed a request.')
     process = CrawlerProcess()
     process.crawl(CompanyProfileSpider)
     process.start()
     return func.HttpResponse(f"This HTTP triggered function executed successfully.")
+
+
+@scraped_stock.schedule(schedule="0 0 0 * * *", arg_name="myTimer1", run_on_startup=True)
+def TimerTriggerScrapedStockData(myTimer1: func.TimerRequest) -> None:
+    if myTimer1.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function started.')
+
+    process = CrawlerProcess()
+    process.crawl(CompanyProfileSpider)
+    process.start()
+
+    logging.info('Python timer trigger function completed successfully.')
