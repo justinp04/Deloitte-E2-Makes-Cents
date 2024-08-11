80abd4f5b5e540186d4590a126b402f152974d12
Thi Van Anh Duong <anht@cbh.com.au>
2024-08-11 18:07:47 +0800

    Finish cron job and upload to azure function

--- CloudScrapy/scraped_stock.py
+++ CloudScrapy/scraped_stock.py
@@ -14,7 +14,7 @@ def ScrapedStockData(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     process = CrawlerProcess()
-    process.crawl(CompanyProfileSpider)
+    # process.crawl(CompanyProfileSpider)
     process.crawl(StockAnalysisSpider)
     # process.crawl(StockNews)
     process.start()
