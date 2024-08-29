28c9a81aa41c08bff054fe5989c80177f8ec9cda
justinp04 <justin.pan688@gmail.com>
2024-08-29 09:35:31 +0800

    Animated newsfeed sidebar button

--- src/components/newsfeed-components/NewsSidebar.js
+++ src/components/newsfeed-components/NewsSidebar.js
@@ -3,8 +3,8 @@
  * Fix:
  *  - Colour
  ************************************************************************************************/
-import React, { useRef } from 'react';
-import { Button, Dropdown, Nav } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Dropdown, Nav, Accordion, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import SearchBar from '../SearchBar';
@@ -15,29 +15,63 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const NewsSidebar = () => {
-    const isOpen = useRef(false); 
-    // Using a ref now so that it persist across renders, without re-rendering the component upon changing
-    // This ensures that we still have our smooth animations
+    const [expandedItem, setExpandedItem] = useState(false);
 
-    const toggleAccordion = () => {
-        isOpen.current = !isOpen.current;
+    const handleToggle = (eventKey) => {
+        setExpandedItem(expandedItem === eventKey ? null : eventKey);
     };
 
     return (
-        <div className="p-0 mt-4">
-            <SearchBar placeholder="Search A Stock" />
-            {/* <ToggleList /> */}
+        <div className="flex flex-direction-column justify-content-start p-0 mt-4">
+            <Container fluid className="m-0 p-0">
+                <SearchBar placeholder="Search A Stock" />
+                <Accordion>
+                    <Accordion.Item eventKey="0" onClick={() => handleToggle("0")}>
+                        <Accordion.Header className="d-inline-flex justify-content-between align-items-centre">
+                            <FontAwesomeIcon
+                                id="stockRecommendations"
+                                icon={faChevronRight}
+                                className={`chevron-icon ${expandedItem === "0" ? 'rotate' : ''}`}
+                            />
+                            <p className="my-0 ps-3">Stock Recommendations</p>
+                        </Accordion.Header>
+                        <Accordion.Body className="ps-4 pe-1">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1" onClick={() => handleToggle("1")}>
+                        <Accordion.Header className="d-inline-flex justify-content-between align-items-centre">
+                            <FontAwesomeIcon
+                                id="stockRecommendations"
+                                icon={faChevronRight}
+                                className={`chevron-icon ${expandedItem === "1" ? 'rotate' : ''}`}
+                            />
+                            <p className="my-0 ps-3">Following</p>
+                        </Accordion.Header>
+                        <Accordion.Body className="ps-4 pe-1">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </Container >
 
-            {/* <Button 
-                className='add-button'>
-            </Button> */}
-
-            <Dropdown align="end" className="me-3 ms-3 add-button" style={{ border: 'none' }}>
+            <Dropdown className="me-3 ms-3 add-button" style={{ border: 'none' }}>
                 <Dropdown.Toggle
                     as={Nav.Link}
                     className="fw-bold p-0 no-caret"
-                    style={{ border: 'none', backgroundColor: 'transparent' }}
-                >
+                    style={{ border: 'none', backgroundColor: 'transparent' }}>
                     <FontAwesomeIcon
                         icon={faPlus}
                         style={{
@@ -45,10 +79,8 @@ const NewsSidebar = () => {
                             width: '2rem',
                             color: 'white'
                         }}
-                        className="align-items-center"
-                    />
+                        className="align-items-center"/>
                 </Dropdown.Toggle>
-
                 <Dropdown.Menu>
                     <Dropdown.Item as={Link} to="">
                         Add new current investment
@@ -61,73 +93,7 @@ const NewsSidebar = () => {
                     </Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
-
-            <div id="accordion" >
-                <div className="card w-300" style={{ border: 'none', backgroundColor: 'transparent' }}>
-                    <div className="card-header ps-0 w-300" id="headingOne" style={{ border: 'none', backgroundColor: 'transparent' }}>
-                        <h5 className="mb-0">
-                            <button
-                                className="accordion-button d-flex align-items-center"
-                                type="button"
-                                data-bs-toggle="collapse"
-                                data-bs-target="#collapseOne"
-                                aria-expanded="true"
-                                aria-controls="collapseOne"
-                                // onClick={toggleAccordion} //toggleAccordian() calls the method, so do not include the parenthesis
-                            >
-                                <FontAwesomeIcon
-                                    icon={isOpen? faChevronRight : faChevronDown}
-                                    className="me-2"
-                                />
-                                <span>Stock Recommendations</span>
-                            </button>
-                        </h5>
-                    </div>
-
-                    <div
-                        id="collapseOne"
-                        className={`collapse ${isOpen ? 'show' : ''}`}
-                        aria-labelledby="headingOne"
-                        data-bs-parent="#accordion"
-                    >
-                        <div className="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                        </div>
-                    </div>
-                </div>
-                <div className="card w-300" style={{ border: 'none', backgroundColor: 'transparent' }}>
-                    <div className="card-header ps-0 w-300" id="headingTwo " style={{ border: 'none', backgroundColor: 'transparent' }}>
-                        <h5 className="mb-0">
-                            <button
-                                className="accordion-button d-flex align-items-center"
-                                type="button"
-                                data-bs-toggle="collapse"
-                                data-bs-target="#collapseTwo"
-                                aria-expanded="false"
-                                aria-controls="collapseTwo"
-                            >
-                                <FontAwesomeIcon
-                                    icon={isOpen ? faChevronDown : faChevronRight}
-                                    className="me-2"
-                                />
-                                <span>Following</span>
-                            </button>
-                        </h5>
-                    </div>
-                    <div
-                        id="collapseTwo"
-                        className="collapse"
-                        aria-labelledby="headingTwo"
-                        data-bs-parent="#accordion"
-                    >
-                        <div className="card-body">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                            <Button></Button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </div >
     );
 }
 export default NewsSidebar;
