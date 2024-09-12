7331c153e2a3c4cc214302fb21a5e2cfa939611d
alysshak <103403958+alysshak@users.noreply.github.com>
2024-09-13 00:58:49 +0800

    changes to query bar and chat bubble

--- Front-end/src/components/stockanalysis-components/QueryInputBar.js
+++ Front-end/src/components/stockanalysis-components/QueryInputBar.js
@@ -7,50 +7,52 @@ import React, { useState } from 'react';
 import '../pages/StockAnalysis.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faTriangleExclamation, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const QueryInputBar = ({ onSendMessage }) => {
-  const [message, setMessage] = useState('');
+	const [message, setMessage] = useState('');
 
-  const handleInputChange = (e) => {
-    setMessage(e.target.value);
-  };
+	const handleInputChange = (e) => {
+		setMessage(e.target.value);
+	};
 
-  // When user requests to send message
-  const handleSend = () => {
-    if (message.trim()) {
-      onSendMessage(message);
-      setMessage(''); // Clear the input after sending
-    }
-  };
+	// When user requests to send message
+	const handleSend = () => {
+		if (message.trim()) {
+		onSendMessage(message);
+		setMessage(''); // Clear the input after sending
+		}
+	};
 
-  // When enter/return key is clicked, message is sent
-  const handleEnterKey = (e) => {
-    if (e.key === 'Enter') {
-      handleSend();
-    }
-  };
+	// When enter/return key is clicked, message is sent
+	const handleEnterKey = (e) => {
+		if (e.key === 'Enter') {
+		handleSend();
+		}
+	};
 
-  return (
-    <div>
-      <div className="query-input-bar">
-        <input
-          type="text"
-          value={message}
-          onChange={handleInputChange}
-          onKeyDown={handleEnterKey}
-          placeholder="Ask a query..."
-          className="input-field px-4"
-        />
-        <button onClick={handleSend} className="send-button">Send</button>
-      </div>
-      <div className='d-flex warning'>
-        <FontAwesomeIcon icon={faTriangleExclamation} />
-        <p>Makes Cents is not a financial advisor, investing is a risky venture and your actions are done at your own risk</p>
-        <FontAwesomeIcon icon={faTriangleExclamation} />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="query-input-bar">
+				<input
+				type="text"
+				value={message}
+				onChange={handleInputChange}
+				onKeyDown={handleEnterKey}
+				placeholder="Ask Gerry a question!"
+				className="input-field px-4"
+				/>
+				<button onClick={handleSend} className="send-button">
+					<FontAwesomeIcon icon={faPaperPlane} />
+				</button>
+			</div>
+			<div className='d-flex warning'>
+				<FontAwesomeIcon icon={faTriangleExclamation} />
+				<p>Makes Cents is not a financial advisor, investing is a risky venture and your actions are done at your own risk</p>
+				<FontAwesomeIcon icon={faTriangleExclamation} />
+			</div>
+		</div>
+	);
 };
 
 export default QueryInputBar;
