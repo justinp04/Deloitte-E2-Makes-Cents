cf5546c7d075d6ee5201c4440e455da3faad2648
alysshak <103403958+alysshak@users.noreply.github.com>
2024-09-23 23:26:26 +0800

    Create NewsSidebarCard.jsx

---
+++ Front-end/src/components/newsfeed-components/NewsSidebarCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const NewsSidebarCard = ({ companyTitle, onClick }) => {
+    return (
+        <Card
+            style={{ cursor: 'pointer', backgroundColor: '#FFFFFF', borderRadius: '0', border: 'none' }}
+            onClick={onClick}
+        >
+            <Card.Body 
+                className='fw-bold' 
+                style={{ fontSize: '75%', textTransform: 'uppercase' }} // Apply uppercase transformation here
+            >
+                {companyTitle}
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default NewsSidebarCard;
