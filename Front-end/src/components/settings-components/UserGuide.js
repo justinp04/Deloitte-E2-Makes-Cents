bde0dba959996d6c7c12124341de0b7df7ad772f
alysshak <103403958+alysshak@users.noreply.github.com>
2024-10-03 01:53:23 +0800

    Added button

--- Front-end/src/components/settings-components/UserGuide.js
+++ Front-end/src/components/settings-components/UserGuide.js
@@ -3,6 +3,12 @@ import React from 'react';
 const UserGuide = () => {
     return (
         <div>
+            <button
+                className="green-btn"
+                style={{width: '150px'}}
+            >
+            Start Tutorial
+            </button>
         </div>
     );
 };
